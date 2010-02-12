8eac038fb6f444aae053d886b0e0856990f6a4f8
matt@mattburns.co.uk <matt@mattburns.co.uk@772687f0-09d7-11df-9f86-67d497c0387b>
2010-02-12 00:31:26 +0000

    Fixed css for new image widths

--- trunk/sitemaker/test/uk/co/mattburns/sitemaker/AlbumMakerTest.java
+++ trunk/sitemaker/test/uk/co/mattburns/sitemaker/AlbumMakerTest.java
@@ -9,7 +9,6 @@ public class AlbumMakerTest {
 
 	@Test
 	public void generateAlbums() throws IOException {
-		
 		new AlbumMaker("ashley", new File("C:/Users/Public/Pictures/2010/2010_01_31_export_2010_02_09"));
 		new AlbumMaker("grice", new File("C:/Users/Public/Pictures/2010/2010_01_24_export_2010_01_25"));
 	}
