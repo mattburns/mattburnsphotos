92f2efd1dd39b15ef531afcf30ab240622aa6109
matt@mattburns.co.uk <matt@mattburns.co.uk@772687f0-09d7-11df-9f86-67d497c0387b>
2010-02-13 23:23:46 +0000

    Minor fixes. portfolio added

--- sitemaker/test/uk/co/mattburns/sitemaker/AlbumMakerTest.java
+++ sitemaker/test/uk/co/mattburns/sitemaker/AlbumMakerTest.java
@@ -11,7 +11,6 @@ public class AlbumMakerTest {
 	public void generateAlbums() throws IOException {
 		new AlbumMaker("ashley2224", "Ashley", new File("C:/Users/Public/Pictures/2010/2010_01_31_export_2010_02_09"));
 		new AlbumMaker("grice", "Grice", new File("C:/Users/Public/Pictures/2010/2010_01_24_export_2010_01_25"));
-		new AlbumMaker("portfolio", "Portfolio", new File("C:/Users/Public/Pictures/website_portfolio"));
-		
+		new AlbumMaker("portfolio", "portfolio", new File("C:/Users/Public/Pictures/website_portfolio"));
 	}
 }
