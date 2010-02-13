c8daaa2844e4e9c69187d8e05a920c4104b8f383
matt@mattburns.co.uk <matt@mattburns.co.uk@772687f0-09d7-11df-9f86-67d497c0387b>
2010-02-13 22:13:16 +0000

    Added prices

--- sitemaker/src/uk/co/mattburns/sitemaker/AlbumMaker.java
+++ sitemaker/src/uk/co/mattburns/sitemaker/AlbumMaker.java
@@ -3,7 +3,6 @@ package uk.co.mattburns.sitemaker;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
