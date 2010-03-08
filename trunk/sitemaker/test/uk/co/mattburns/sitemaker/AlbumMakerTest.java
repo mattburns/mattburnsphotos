fe32f640aa496c7dc61bd25decce067f0dd0a622
matt@mattburns.co.uk <matt@mattburns.co.uk@772687f0-09d7-11df-9f86-67d497c0387b>
2010-03-08 23:38:28 +0000

    Added tony's album

--- trunk/sitemaker/test/uk/co/mattburns/sitemaker/AlbumMakerTest.java
+++ trunk/sitemaker/test/uk/co/mattburns/sitemaker/AlbumMakerTest.java
@@ -13,5 +13,6 @@ public class AlbumMakerTest {
 		new AlbumMaker("ashley2224", "Ashley", new File("C:/Documents and Settings/matt/My Documents/My Pictures/2010/2010_01_31_export_2010_02_09"));
 		new AlbumMaker("grice", "Grice", new File("C:/Documents and Settings/matt/My Documents/My Pictures/2010/2010_01_24_export_2010_01_25"));
 		new AlbumMaker("grandad", "Grandad", new File("C:/Documents and Settings/matt/My Documents/My Pictures/2010/2010_01_15_export"));
+		new AlbumMaker("stleonards", "Tony and Claire", new File("C:/Documents and Settings/matt/My Documents/My Pictures/2009/2009_12_28_export"));
 	}
 }
