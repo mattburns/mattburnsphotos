ff7e2de83687ed706a6cae3d05effc6cd8b27be4
matt@mattburns.co.uk <matt@mattburns.co.uk@772687f0-09d7-11df-9f86-67d497c0387b>
2010-03-08 22:46:45 +0000

    Styled cart like rest of site

--- sitemaker/test/uk/co/mattburns/sitemaker/AlbumMakerTest.java
+++ sitemaker/test/uk/co/mattburns/sitemaker/AlbumMakerTest.java
@@ -9,8 +9,9 @@ public class AlbumMakerTest {
 
 	@Test
 	public void generateAlbums() throws IOException {
+		new AlbumMaker(AlbumMaker.PORTFOLIO, AlbumMaker.PORTFOLIO, new File("C:/Documents and Settings/matt/My Documents/My Pictures/website_portfolio"));
 		new AlbumMaker("ashley2224", "Ashley", new File("C:/Documents and Settings/matt/My Documents/My Pictures/2010/2010_01_31_export_2010_02_09"));
 		new AlbumMaker("grice", "Grice", new File("C:/Documents and Settings/matt/My Documents/My Pictures/2010/2010_01_24_export_2010_01_25"));
-		new AlbumMaker(AlbumMaker.PORTFOLIO, AlbumMaker.PORTFOLIO, new File("C:/Documents and Settings/matt/My Documents/My Pictures/website_portfolio"));
+		new AlbumMaker("grandad", "Grandad", new File("C:/Documents and Settings/matt/My Documents/My Pictures/2010/2010_01_15_export"));
 	}
 }
