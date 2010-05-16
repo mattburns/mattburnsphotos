4be14d1c8864b62ddd41389d7895c70ece67d5a9
matt@mattburns.co.uk <matt@mattburns.co.uk@772687f0-09d7-11df-9f86-67d497c0387b>
2010-05-16 20:59:26 +0000

    Removed disqus comment integration. Was kinda broken anyway.

--- trunk/sitemaker/src/uk/co/mattburns/sitemaker/AlbumMaker.java
+++ trunk/sitemaker/src/uk/co/mattburns/sitemaker/AlbumMaker.java
@@ -14,192 +14,186 @@ import java.util.List;
 
 public class AlbumMaker {
 
-	public static final String PORTFOLIO = "portfolio";
-	private static final String IMAGEMAGICK = "C:/Program Files/ImageMagick-6.6.0-Q16/";
-	private static final String WATERMARK = "\"C:\\svnrepos\\mattburnsphotos\\images\\watermark.png\"";
-	private static final String CONVERT = "\"" + IMAGEMAGICK + "convert\"";
-	private static final String COMPOSITE = "\"" + IMAGEMAGICK + "composite\"";
-	private static final File GENERATED_DIR = new File("C:/svnrepos/mattburnsphotos/generated");
-	private static final File IMAGES_DIR = new File("C:/svnrepos/mattburnsphotos/images");
-	private static final File JAVASCRIPT_DIR = new File("C:/svnrepos/mattburnsphotos/javascript");
-	private static final File CSS_DIR = new File("C:/svnrepos/mattburnsphotos/css");
-	private static final File TEMPLATE_HTML = new File("C:/svnrepos/mattburnsphotos/sitemaker/templates/album.html");
-	private static final File HOME_HTML = new File("C:/svnrepos/mattburnsphotos/index.html");
-	private static final File CONTACT_HTML = new File("C:/svnrepos/mattburnsphotos/contact.html");
-	private static final File CREDITS_HTML = new File("C:/svnrepos/mattburnsphotos/credits.html");
-	private static final File CLIENTS_HTML = new File("C:/svnrepos/mattburnsphotos/clients.html");
-	private static final File PRICES_HTML = new File("C:/svnrepos/mattburnsphotos/prices.html");
+    public static final String PORTFOLIO = "portfolio";
+    private static final String IMAGEMAGICK = "C:/Program Files/ImageMagick-6.6.0-Q16/";
+    private static final String WATERMARK = "\"C:\\svnrepos\\mattburnsphotos\\images\\watermark.png\"";
+    private static final String CONVERT = "\"" + IMAGEMAGICK + "convert\"";
+    private static final String COMPOSITE = "\"" + IMAGEMAGICK + "composite\"";
+    private static final File GENERATED_DIR = new File("C:/svnrepos/mattburnsphotos/generated");
+    private static final File IMAGES_DIR = new File("C:/svnrepos/mattburnsphotos/images");
+    private static final File JAVASCRIPT_DIR = new File("C:/svnrepos/mattburnsphotos/javascript");
+    private static final File CSS_DIR = new File("C:/svnrepos/mattburnsphotos/css");
+    private static final File TEMPLATE_HTML = new File("C:/svnrepos/mattburnsphotos/sitemaker/templates/album.html");
+    private static final File HOME_HTML = new File("C:/svnrepos/mattburnsphotos/index.html");
+    private static final File CONTACT_HTML = new File("C:/svnrepos/mattburnsphotos/contact.html");
+    private static final File CREDITS_HTML = new File("C:/svnrepos/mattburnsphotos/credits.html");
+    private static final File CLIENTS_HTML = new File("C:/svnrepos/mattburnsphotos/clients.html");
+    private static final File PRICES_HTML = new File("C:/svnrepos/mattburnsphotos/prices.html");
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-		String albumName = args[0];
-		File photoSourceDirectory = new File(args[1]);
+        String albumName = args[0];
+        File photoSourceDirectory = new File(args[1]);
 
-		if (!photoSourceDirectory.exists()
-				|| !photoSourceDirectory.isDirectory()) {
-			throw new RuntimeException("arg 1 must be source photo dir: "
-					+ args[1]);
-		}
+        if (!photoSourceDirectory.exists() || !photoSourceDirectory.isDirectory()) {
+            throw new RuntimeException("arg 1 must be source photo dir: " + args[1]);
+        }
 
-		new AlbumMaker(albumName, albumName, photoSourceDirectory);
-	}
+        new AlbumMaker(albumName, albumName, photoSourceDirectory);
+    }
 
-	public AlbumMaker(String albumID, String albumName, File photoSourceDirectory) throws IOException {
-		GENERATED_DIR.mkdirs();
-		copyFile(CSS_DIR, new File(GENERATED_DIR, "css"));
-		copyFile(IMAGES_DIR, new File(GENERATED_DIR, "images"));
-		copyFile(JAVASCRIPT_DIR, new File(GENERATED_DIR, "javascript"));
-		copyFile(HOME_HTML, GENERATED_DIR);
-		copyFile(CONTACT_HTML, GENERATED_DIR);
-		copyFile(PRICES_HTML, GENERATED_DIR);
-		copyFile(CLIENTS_HTML, GENERATED_DIR);
-		copyFile(CREDITS_HTML, GENERATED_DIR);
+    public AlbumMaker(String albumID, String albumName, File photoSourceDirectory) throws IOException {
+        if (!photoSourceDirectory.exists()) {
+            throw new IllegalArgumentException("Source directory not found: " + photoSourceDirectory);
+        }
+        GENERATED_DIR.mkdirs();
+        copyFile(CSS_DIR, new File(GENERATED_DIR, "css"));
+        copyFile(IMAGES_DIR, new File(GENERATED_DIR, "images"));
+        copyFile(JAVASCRIPT_DIR, new File(GENERATED_DIR, "javascript"));
+        copyFile(HOME_HTML, GENERATED_DIR);
+        copyFile(CONTACT_HTML, GENERATED_DIR);
+        copyFile(PRICES_HTML, GENERATED_DIR);
+        copyFile(CLIENTS_HTML, GENERATED_DIR);
+        copyFile(CREDITS_HTML, GENERATED_DIR);
 
-		File clientAlbumsDirectory = new File(GENERATED_DIR, "c");
+        File clientAlbumsDirectory = new File(GENERATED_DIR, "c");
 
-		File albumDir = new File(clientAlbumsDirectory, albumID);
-		albumDir.mkdirs();
+        File albumDir = new File(clientAlbumsDirectory, albumID);
+        albumDir.mkdirs();
 
-		List<File> sourcePhotos = new ArrayList<File>();
-		for (File photo : photoSourceDirectory.listFiles()) {
-			if (photo.getName().toLowerCase().endsWith("jpg") || photo.getName().toLowerCase().endsWith("jpeg")) {
-				sourcePhotos.add(photo);
-			}
-		}
+        List<File> sourcePhotos = new ArrayList<File>();
+        for (File photo : photoSourceDirectory.listFiles()) {
+            if (photo.getName().toLowerCase().endsWith("jpg") || photo.getName().toLowerCase().endsWith("jpeg")) {
+                sourcePhotos.add(photo);
+            }
+        }
 
-		makeHtml(albumID, albumName, albumDir, sourcePhotos);
-		makeThumbs(albumDir, sourcePhotos);
-		makeMediumPhotos(albumDir, sourcePhotos);
+        makeHtml(albumID, albumName, albumDir, sourcePhotos);
+        makeThumbs(albumDir, sourcePhotos);
+        makeMediumPhotos(albumDir, sourcePhotos);
 
-		System.out.println("Finished " + albumName + " album :)");
-	}
+        System.out.println("Finished " + albumName + " album :)");
+    }
 
-	private void makeHtml(String albumID, String albumName, File albumDir, List<File> sourcePhotos) throws IOException {
-		BufferedReader reader = new BufferedReader(new FileReader(TEMPLATE_HTML));
-		FileWriter writer = new FileWriter(new File(albumDir, "index.html"));
-		
-		String line = reader.readLine();
-		while (line != null) {
-			if (albumID.equals(PORTFOLIO) && line.contains("album-only start")) {
-				while (!line.contains("album-only stop")) {
-					line = reader.readLine();
-				}
-				line = reader.readLine();
-			}
-			line = line.replaceAll("albumIDTag", albumID);
-			line = line.replaceAll("albumNameTag", albumName);
-			if (line.contains("photo repeat start")) {
-				StringBuilder sb = new StringBuilder();
-				line = reader.readLine();
-				while (!line.contains("photo repeat stop")) {
-					sb.append(line + "\n");
-					line = reader.readLine();
-				}
-				line = reader.readLine();
-				
-				for (File photo : sourcePhotos) {
-					writer.write(sb.toString().replaceAll("photoTag", photo.getName()));
-				}
-			}
-			writer.write(line + "\n");
-			line = reader.readLine();
-		}
-		
-		File thumbsDir = new File(albumDir, "thumbs");
-		thumbsDir.mkdirs();
-		reader.close();
-		writer.close();
-		
-	}
-	
-	private void makeThumbs(File albumDir, List<File> sourcePhotos) {
-		File thumbsDir = new File(albumDir, "thumbs");
-		thumbsDir.mkdirs();
+    private void makeHtml(String albumID, String albumName, File albumDir, List<File> sourcePhotos) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(TEMPLATE_HTML));
+        FileWriter writer = new FileWriter(new File(albumDir, "index.html"));
 
-		for (File photo : sourcePhotos) {
-			File destinationFile = new File(thumbsDir, photo.getName());
-			if (!destinationFile.exists()) {
-				String command = CONVERT
-						+ " \""
-						+ photo.getAbsolutePath()
-						+ "\" -resize 50x50^^ -gravity center -strip -quality 75 -extent 50x50 \""
-						+ destinationFile.getAbsolutePath() + "\"";
-				runCommand(command);
-			}
-		}
-	}
+        String line = reader.readLine();
+        while (line != null) {
+            if (albumID.equals(PORTFOLIO) && line.contains("album-only start")) {
+                while (!line.contains("album-only stop")) {
+                    line = reader.readLine();
+                }
+                line = reader.readLine();
+            }
+            line = line.replaceAll("albumIDTag", albumID);
+            line = line.replaceAll("albumNameTag", albumName);
+            if (line.contains("photo repeat start")) {
+                StringBuilder sb = new StringBuilder();
+                line = reader.readLine();
+                while (!line.contains("photo repeat stop")) {
+                    sb.append(line + "\n");
+                    line = reader.readLine();
+                }
+                line = reader.readLine();
 
-	private void makeMediumPhotos(File albumDir, List<File> sourcePhotos) {
-		File mediumDir = new File(albumDir, "medium");
-		mediumDir.mkdirs();
+                for (File photo : sourcePhotos) {
+                    writer.write(sb.toString().replaceAll("photoTag", photo.getName()));
+                }
+            }
+            writer.write(line + "\n");
+            line = reader.readLine();
+        }
 
-		for (File photo : sourcePhotos) {
-			File mediumFile = new File(mediumDir, photo.getName());
-			if (!mediumFile.exists()) {
-				String command = CONVERT + " \"" + photo.getAbsolutePath()
-						+ "\" -resize 600x400 \""
-						+ mediumFile.getAbsolutePath() + "\"";
-				runCommand(command);
-				waterMarkPhoto(mediumFile);
-			}
-		}
-	}
-	
-	private void waterMarkPhoto(File photo) {
-		File stampedFile = new File(photo.getAbsolutePath() + ".stamped.jpg");
-		String command = COMPOSITE
-				+ " -gravity south -geometry +0+10 -strip -quality 85 " + WATERMARK + " \""
-				+ photo.getAbsolutePath() + "\" \""
-				+ stampedFile.getAbsolutePath() + "\"";
-		runCommand(command);
-		String originalName = photo.getAbsolutePath();
-		photo.delete();
-		stampedFile.renameTo(new File(originalName));
-	}
+        File thumbsDir = new File(albumDir, "thumbs");
+        thumbsDir.mkdirs();
+        reader.close();
+        writer.close();
 
-	private void runCommand(String command) {
-		Process child;
-		try {
-			System.out.println("Running : " + command);
-			child = Runtime.getRuntime().exec(command);
-			child.waitFor();
-		} catch (Exception e) {
-			throw new RuntimeException("command failed: " + command, e);
-		}
-	}
-	
+    }
+
+    private void makeThumbs(File albumDir, List<File> sourcePhotos) {
+        File thumbsDir = new File(albumDir, "thumbs");
+        thumbsDir.mkdirs();
+
+        for (File photo : sourcePhotos) {
+            File destinationFile = new File(thumbsDir, photo.getName());
+            if (!destinationFile.exists()) {
+                String command = CONVERT + " \"" + photo.getAbsolutePath()
+                        + "\" -resize 50x50^^ -gravity center -strip -quality 75 -extent 50x50 \""
+                        + destinationFile.getAbsolutePath() + "\"";
+                runCommand(command);
+            }
+        }
+    }
+
+    private void makeMediumPhotos(File albumDir, List<File> sourcePhotos) {
+        File mediumDir = new File(albumDir, "medium");
+        mediumDir.mkdirs();
+
+        for (File photo : sourcePhotos) {
+            File mediumFile = new File(mediumDir, photo.getName());
+            if (!mediumFile.exists()) {
+                String command = CONVERT + " \"" + photo.getAbsolutePath() + "\" -resize 600x400 \""
+                        + mediumFile.getAbsolutePath() + "\"";
+                runCommand(command);
+                waterMarkPhoto(mediumFile);
+            }
+        }
+    }
+
+    private void waterMarkPhoto(File photo) {
+        File stampedFile = new File(photo.getAbsolutePath() + ".stamped.jpg");
+        String command = COMPOSITE + " -gravity south -geometry +0+10 -strip -quality 85 " + WATERMARK + " \""
+                + photo.getAbsolutePath() + "\" \"" + stampedFile.getAbsolutePath() + "\"";
+        runCommand(command);
+        String originalName = photo.getAbsolutePath();
+        photo.delete();
+        stampedFile.renameTo(new File(originalName));
+    }
+
+    private void runCommand(String command) {
+        Process child;
+        try {
+            System.out.println("Running : " + command);
+            child = Runtime.getRuntime().exec(command);
+            child.waitFor();
+        } catch (Exception e) {
+            throw new RuntimeException("command failed: " + command, e);
+        }
+    }
+
     // If targetLocation does not exist, it will be created.
-    public void copyFile(File sourceLocation , File targetLocation)
-    throws IOException {
-    	if (sourceLocation.getName().equals(".svn") ){
-    		return;
-    	}
-    	
+    public void copyFile(File sourceLocation, File targetLocation) throws IOException {
+        if (sourceLocation.getName().equals(".svn")) {
+            return;
+        }
+
         if (sourceLocation.isDirectory()) {
             if (!targetLocation.exists()) {
                 targetLocation.mkdir();
             }
-            
+
             String[] children = sourceLocation.list();
-            for (int i=0; i<children.length; i++) {
-                copyFile(new File(sourceLocation, children[i]),
-                        new File(targetLocation, children[i]));
+            for (int i = 0; i < children.length; i++) {
+                copyFile(new File(sourceLocation, children[i]), new File(targetLocation, children[i]));
             }
         } else {
             if (targetLocation.isDirectory()) {
-            	copyFile(sourceLocation, new File(targetLocation, sourceLocation.getName()));
+                copyFile(sourceLocation, new File(targetLocation, sourceLocation.getName()));
             } else {
-	            InputStream in = new FileInputStream(sourceLocation);
-	            OutputStream out = new FileOutputStream(targetLocation);
-	            
-	            // Copy the bits from instream to outstream
-	            byte[] buf = new byte[1024];
-	            int len;
-	            while ((len = in.read(buf)) > 0) {
-	                out.write(buf, 0, len);
-	            }
-	            in.close();
-	            out.close();
+                InputStream in = new FileInputStream(sourceLocation);
+                OutputStream out = new FileOutputStream(targetLocation);
+
+                // Copy the bits from instream to outstream
+                byte[] buf = new byte[1024];
+                int len;
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
+                in.close();
+                out.close();
             }
         }
     }
