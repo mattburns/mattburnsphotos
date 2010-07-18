99794502a2027f076f3149dd2921a3d96a094fa2
matt@mattburns.co.uk <matt@mattburns.co.uk@772687f0-09d7-11df-9f86-67d497c0387b>
2010-07-18 23:13:45 +0000

    Allow different price plans. Not an ideal solution but it's a technical debt I'm willing to have at 00:13 on a Sunday night.

--- trunk/sitemaker/test/uk/co/mattburns/sitemaker/AlbumMakerTest.java
+++ trunk/sitemaker/test/uk/co/mattburns/sitemaker/AlbumMakerTest.java
@@ -10,19 +10,34 @@ public class AlbumMakerTest {
     @Test
     public void generateAlbums() throws IOException {
         new AlbumMaker(AlbumMaker.PORTFOLIO, AlbumMaker.PORTFOLIO, new File(
-                "C:/Documents and Settings/matt/My Documents/My Pictures/website_portfolio"));
+                "C:/Documents and Settings/matt/My Documents/My Pictures/website_portfolio"), PricePlan.NONE);
         new AlbumMaker("ashley2224", "Ashley", new File(
-                "C:/Documents and Settings/matt/My Documents/My Pictures/2010/2010_01_31_DVD_Ashley/edited"));
+                "C:/Documents and Settings/matt/My Documents/My Pictures/2010/2010_01_31_DVD_Ashley/edited"),
+                PricePlan.PORTRAIT);
         new AlbumMaker("grice", "Grice", new File(
-                "C:/Documents and Settings/matt/My Documents/My Pictures/2010/2010_01_24_export_2010_01_25"));
+                "C:/Documents and Settings/matt/My Documents/My Pictures/2010/2010_01_24_export_2010_01_25"),
+                PricePlan.PORTRAIT);
         new AlbumMaker("grandad", "Grandad", new File(
-                "C:/Documents and Settings/matt/My Documents/My Pictures/2010/2010_01_15_export"));
-        new AlbumMaker("stleonards", "Tony and Claire", new File(
-                "C:/Documents and Settings/matt/My Documents/My Pictures/2009/2009_12_28_DVD_tonys_wedding/edited"));
+                "C:/Documents and Settings/matt/My Documents/My Pictures/2010/2010_01_15_export"), PricePlan.PORTRAIT);
+        new AlbumMaker(
+                "stleonards",
+                "Tony and Claire",
+                new File(
+                        "C:/Documents and Settings/matt/My Documents/My Pictures/2009/2009_12_28_DVD_tonys_wedding/disc01/edited"),
+                PricePlan.PORTRAIT);
         new AlbumMaker("aidan313", "Aidan's Baptism", new File(
-                "C:/Documents and Settings/matt/My Documents/My Pictures/2010/2010-03-13_export"));
+                "C:/Documents and Settings/matt/My Documents/My Pictures/2010/2010-03-13_export"), PricePlan.PORTRAIT);
         new AlbumMaker("sophie2010", "Sophie's Baptism", new File(
-                "C:/Documents and Settings/matt/My Documents/My Pictures/2010/2010-04-24_sophie_DVD/edited"));
+                "C:/Documents and Settings/matt/My Documents/My Pictures/2010/2010-04-24_sophie_DVD/edited"),
+                PricePlan.PORTRAIT);
+        new AlbumMaker("skatecal20103147940776278", "Longboard Calendar", new File(
+                "C:/Documents and Settings/matt/My Documents/My Pictures/2010/2010-05-30_export_gimped"),
+                PricePlan.PORTRAIT);
+        new AlbumMaker("skatecalalt20103147940776278", "Longboard Calendar alternatives", new File(
+                "C:/Documents and Settings/matt/My Documents/My Pictures/2010/2010-05-30_alternatives"),
+                PricePlan.PORTRAIT);
+        new AlbumMaker("kcps2010", "Kings Court Leaving Prom", new File(
+                "C:/Documents and Settings/matt/My Documents/My Pictures/2010/2010-07-15_export"), PricePlan.SCHOOL);
 
     }
 }
