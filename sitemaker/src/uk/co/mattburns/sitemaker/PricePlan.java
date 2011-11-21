b171093ff17d6553a9e8ff04ab0aef4958767589
mattburnsphotos@gmail.com <mattburnsphotos@gmail.com@772687f0-09d7-11df-9f86-67d497c0387b>
2011-11-21 20:53:16 +0000

    updated prices

--- sitemaker/src/uk/co/mattburns/sitemaker/PricePlan.java
+++ sitemaker/src/uk/co/mattburns/sitemaker/PricePlan.java
@@ -9,19 +9,25 @@ public enum PricePlan {
     private List<Product> products;
     private Product defaultProduct;
 
-    private final Product eightBySixMounted = new Product("7.00",
-            "8\"x6\" mounted", "8x6m");
     private final Product sixByFour = new Product("1.00", "6\"x4\"", "6x4");
     private final Product sixByFourMounted = new Product("3.00",
             "6\"x4\" mounted", "6x4m");
     private final Product nineBySix = new Product("5.00", "9\"x6\"", "9x6");
     private final Product eightBySix = new Product("5.00", "8\"x6\"", "8x6");
+    private final Product eightBySixMounted = new Product("7.00",
+            "8\"x6\" mounted", "8x6m");
     private final Product eightBySixFramed = new Product("20.00",
+            "8\"x6\" mounted and framed", "8x6f");
+    private final Product eightBySixFramed2011 = new Product("35.00",
             "8\"x6\" mounted and framed", "8x6f");
     private final Product fifteenByTen = new Product("15.00", "15\"x10\"",
             "15x10");
+    private final Product fifteenByTen2011 = new Product("25.00", "15\"x10\"",
+            "15x10");
     private final Product thirtyByTwenty = new Product("40.00", "30\"x20\"",
             "30x20");
+    private final Product thirtyByTwenty2011 = new Product("90.00",
+            "30\"x20\"", "30x20");
     private final Product dvd50Quid = new Product("50.00",
             "albumNameTag album on DVD", "albumNameTag album on DVD");
     private final Product dvd100Quid = new Product("100.00",
@@ -86,9 +92,9 @@ public enum PricePlan {
                 products.add(nineBySix);
                 products.add(eightBySix);
                 products.add(eightBySixMounted);
-                products.add(fifteenByTen);
-                products.add(eightBySixFramed);
-                products.add(thirtyByTwenty);
+                products.add(fifteenByTen2011);
+                products.add(eightBySixFramed2011);
+                products.add(thirtyByTwenty2011);
                 products.add(dvd100Quid);
                 break;
             case SCHOOL:
